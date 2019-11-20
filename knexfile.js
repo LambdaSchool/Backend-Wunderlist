9bf935eef8f2adb037ce3648428371021fcf2930
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-20 12:13:41 -0500

    Added production settings to the knex file

--- knexfile.js
+++ knexfile.js
@@ -9,12 +9,24 @@ module.exports = {
     },
     seeds: { directory: './database/seeds' },
   },
+  
   testing: {
     client: 'sqlite3',
     connection: {
       filename: './database/test.db3',
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
     },
